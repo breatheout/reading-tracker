2bf26fa5875f7760de616172b5e6590a076cf3f4
breatheout <breatheout18@gmail.com>
2022-05-09 18:03:27 +0100

    make it better

--- src/app/config/db.config.js
+++ src/app/config/db.config.js
@@ -7,11 +7,25 @@ module.exports = mysql.createConnection({
   database: "readingtracker",
 });*/
 
-const Sequelize = require("sequelize");
+/*const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("readingtracker", "root", null, {
   host: "localhost",
   dialect: "mysql",
 });
+
+module.exports = sequelize;*/
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  "heroku_7643ec736354xxx",
+  "b7e2437887xxxa",
+  "200xxx6",
+  {
+    host: "heroku_7643ec736354xxx",
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
