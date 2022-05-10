9bb8e262643af90fbf5549aa1bd81edb3d721ee1
breatheout <breatheout18@gmail.com>
2022-05-10 20:19:27 +0100

    make it better

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    methods: ["GET, POST, PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
