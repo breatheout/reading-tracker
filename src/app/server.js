9f2c56ac27fce933a971491aaceb25e73e1c5ce3
breatheout <breatheout18@gmail.com>
2022-05-10 17:38:40 +0100

    make it better

--- src/app/server.js
+++ src/app/server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static("./dist/reading-tracker"));
+app.use(express.static("./dist"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join("./dist/index.html"));
+});
+//app.use(express.static("./dist/reading-tracker"));
 /*app.get("/*", function (req, res) {
   res.sendFile(path.join("./dist/reading-tracker/index.html"));
 });*/
